e172332d6b66f64762487b7c7843109a065adbdc
Phuthuyxam <48133351+Phuthuyxam@users.noreply.github.com>
2021-09-01 15:45:19 +0700

    update code

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # clone code
-cd src/src_source
+# cd src/src_source
 
-git clean -xdf
-git reset --hard
-git checkout develop
-git pull
-
-
-cd ../src_deploy
-git clean -xdf
-git reset --hard
-git checkout deploy
-git pull
+# git clean -xdf
+# git reset --hard
+# git checkout develop
+# git pull
 
 
+# cd ../src_deploy
+# git clean -xdf
+# git reset --hard
+# git checkout deploy
+# git pull
 
-db.sh
+chmod +x db.sh
+chmod +x php.sh
+chmod +x apache.sh
 
-php.sh
-
-apache.sh
+../db.sh
+../php.sh
+../apache.sh
